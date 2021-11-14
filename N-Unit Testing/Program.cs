0de9cc678ede5dd14454c3b6fe4e14f30e8fabe3
Harsh Singh <kmrharsh50@gmail.com>
2021-11-14 10:09:05 +0530

    [Harsh]Added:Vending Machine N_Unit_testing Features Implemented

---
+++ N-Unit Testing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace N_Unit_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the NUnit Program \n 1. Vending Machine \n 2. Exit \n ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Amount to Change");
+                        int amount = Convert.ToInt32(Console.ReadLine());
+                        VendingMachine vm = new VendingMachine();
+                        vm.Calculate(amount);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ N-Unit Testing/VendingMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace N_Unit_Testing
+{
+    public class VendingMachine
+    {
+        int[] notes = { 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
+        int[] arr = new int[9];
+        public void Calculate(int amount)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    arr[i] = amount / notes[i];
+                    amount = amount - arr[i] * notes[i];
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + ":" + arr[i]);
+                }
+            }
+        }
+    }
+
+}
